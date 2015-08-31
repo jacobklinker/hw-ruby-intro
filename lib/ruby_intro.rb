595acd2449a8eb9c96d65a40dcc7ede30df50b0b
jacobklinker <jacob-klinker@uiowa.edu>
2015-08-31 17:04:52 +0000

    adhere to ruby style guidelines

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,41 +1,34 @@
 # Part 1
 
-def sum arr
-  sum = 0
-  arr.each { |x| sum += x }
-  sum
+def sum(arr)
+  arr.inject(0, &:+)
 end
 
-def max_2_sum arr
+def max_2_sum(arr)
   sum arr.sort.reverse.take(2)
 end
 
-def sum_to_n? arr, n
+def sum_to_n?(arr, n)
   arr.combination(2).to_a.each do |x,y|
-    if (x+y == n)
-      return true
-    end
+    return true if (x + y) == n
   end
   
-  return false
+  false
 end
 
 # Part 2
 
 def hello(name)
-  return "Hello, #{name}"
+  "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  return s.downcase.match(/^(?=[^aeiou])(?=[a-z])/)
+def starts_with_consonant?(s)
+  s.downcase.match(/^(?=[^aeiou])(?=[a-z])/)
 end
 
-def binary_multiple_of_4? s
-  if s.length == 1
-    return s == '0'
-  else
-    return s.match(/^[10]*00$/)
-  end
+def binary_multiple_of_4?(s)
+  return s == '0' if s.length == 1
+  return s.match(/^[10]*00$/)
 end
 
 # Part 3
@@ -44,13 +37,11 @@ class BookInStock
   attr_accessor :isbn
   attr_accessor :price
   def initialize(isbn, price)
-    if (isbn.length == 0 || price <= 0)
-      raise ArgumentError.new("Invalid params")
-    end
+    raise ArgumentError.new("Invalid params") if isbn.length == 0 || price <= 0
     @isbn = isbn
     @price = price
   end
   def price_as_string
-    return "$%0.2f" % @price
+    "$%0.2f" % @price
   end
 end
